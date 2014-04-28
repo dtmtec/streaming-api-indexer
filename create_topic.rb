76951b423a1e2e1d23bf548771883ff12793387c
Vicente Mundim <vicente.mundim@gmail.com>
2014-04-28 13:13:34 -0300

    Adding configuration to create topic

--- create_topic.rb
+++ create_topic.rb
@@ -2,6 +2,12 @@ require 'bundler/setup'
 
 Bundler.require :default
 
+Restforce.configure do |config|
+  config.compress = true
+  config.instance_url = ENV['SALESFORCE_INSTANCE_URL'] || 'https://na10.salesforce.com'
+  config.api_version = '29.0'
+end
+
 client = Restforce.new
 client.create! 'PushTopic', {
   ApiVersion: '29.0',
